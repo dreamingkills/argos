e1b0205acdf7729fb1eaf57c0d3b16994d3b4506
dreamingkills <realrtfl@gmail.com>
2022-05-28 13:34:05 -0400

    redacted api base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import { GazelleTracker, Tracker } from "argos";
 
 export const CONSTANTS = {
   GAZELLE_BASE_URLS: {
-    red: "https://redacted.ch",
+    red: process.env.REDACTED_API_BASE_URL,
     ops: "https://orpheus.network",
   } as { [key in GazelleTracker]: string },
   TRACKER_FULL_NAME: {
