a7b3999b1fb9657e34d501bec62ce5ddb2632778
dreamingkills <realrtfl@gmail.com>
2022-06-09 03:33:12 -0400

    fix debounce on initial run

--- src/struct/job.ts
+++ src/struct/job.ts
@@ -102,11 +102,11 @@ export class DownloadJob extends Job {
       isEtc: this.isEtc,
     });
 
-    let currentProgress: number = 0;
+    let currentProgress: number | undefined;
 
     while (torrent.progress < 1) {
       const _torrent = (await getTorrent(torrent.hash))!;
-      if (_torrent.progress === currentProgress) continue;
+      if (currentProgress && _torrent.progress === currentProgress) continue;
 
       currentProgress = _torrent.progress;
       torrent = _torrent;
